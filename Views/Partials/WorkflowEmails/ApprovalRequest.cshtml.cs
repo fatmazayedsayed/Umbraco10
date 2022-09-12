5e6750f3a2d951f91b86d50effe5e0a5944d9b36
Fatma zayed <Fatma.Sayed@gbrands.com>
2022-09-12 15:29:26 +0200

    Govern List

---
+++ Views/Partials/WorkflowEmails/ApprovalRequest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbracoProject1.Views.Partials.WorkflowEmails
+{
+    public class ApprovalRequestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
